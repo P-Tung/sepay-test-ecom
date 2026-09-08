38d42591b64aa7a8cf8e2c01b9fd6bc4988a2cac
Tung Pham <tungpham2109@gmail.com>
2026-09-09 00:29:08 +0700

    Add optional demo database seeding

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,6 +12,9 @@ fi
 
 php artisan config:clear
 php artisan migrate --force
+if [ "${SEED_DATABASE:-false}" = "true" ]; then
+    php artisan db:seed --force
+fi
 php artisan storage:link || true
 
 exec "$@"
